4e167d08bceb689343783d65d33c34dafcb52c3b
lborgesc <lborgesc@outlook.com>
2021-02-26 17:15:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { ExperienceBar } from './Components/ExperienceBar';
+import './Style/global.css'
+
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  )};
+
+export default App;
